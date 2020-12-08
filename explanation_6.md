fa6b73d8c5901d367986a6c69aad5be7779c6340
Vinicius <vinc-analist@hotmail.com>
2020-12-07 22:21:09 -0300

    finishing problem and explanation 6

---
+++ explanation_6.md
@@ -0,0 +1,11 @@
+# Problem 6:  Unsorted Integer Array
+### Explanation
+### Design Decisions:
+To find the max and min number in a array, it's necessary to create variables with maximum and minimum int values respectively
+and at the first iteration the variables values will change to the first element of the array. As the algorithm continue to proccess the array, it will find the maximum and minimum value. 
+
+### Time Complexity:
+This solution runs in linear time **O(n)**, n means the array lenght
+
+### Space Complexity:
+The Space Complexity takes constant time  **O(1)**.
---
+++ problem_6.py
@@ -0,0 +1,46 @@
+import sys
+import random
+
+def get_min_max(ints):
+    """
+    Return a tuple(min, max) out of list of unsorted integers.
+
+    Args:
+       ints(list): list of integers containing one or more integers
+    """
+
+    if len(ints) == 0:
+        return ()
+        
+    min = sys.maxsize
+    max = -sys.maxsize - 1
+    index = 0
+
+    while index < len(ints):
+        if ints[index] > max:
+            max = ints[index]
+
+        if ints[index] < min:
+            min = ints[index]
+
+        index+=1
+    
+    return min, max
+
+## Example Test Case of Integers
+
+l1 = [i for i in range(0, 10)]  # a list containing 0 - 9
+random.shuffle(l1)
+print ("Pass" if ((0, 9) == get_min_max(l1)) else "Fail")
+
+l2 = [i for i in range(10, 100)]  # a list containing 10 - 99
+random.shuffle(l2)
+print ("Pass" if ((10, 99) == get_min_max(l2)) else "Fail")
+
+l3 = [i for i in range(-10, 1)]  # a list containing -10 - 0
+random.shuffle(l3)
+print ("Pass" if ((-10, 0) == get_min_max(l3)) else "Fail")
+
+l4 = []  # a list containing nothing
+random.shuffle(l4)
+print ("Pass" if (() == get_min_max(l4)) else "Fail")
